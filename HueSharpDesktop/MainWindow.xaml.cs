d986f2cc9fd02a256d7b65c44801dfaa53a5ec05
Jesse <jelloeater@gmail.com>
2018-12-31 00:05:55 -0500

    Added Start for GUI code

--- HueSharpDesktop/MainWindow.xaml.cs
+++ HueSharpDesktop/MainWindow.xaml.cs
@@ -23,5 +23,22 @@ namespace HueSharpDesktop
         {
             InitializeComponent();
         }
+
+        private void Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            //var LightList = libs.Lights.GetLightList;
+            for (int i = 0; i < 5; i++)
+            {
+
+
+                System.Windows.Controls.Button newBtn = new Button();
+
+                newBtn.Content = i.ToString();
+                newBtn.Name = "Button" + i.ToString();
+
+                mainSubPannel.Children.Add(newBtn);
+
+            }
+        }
     }
 }
